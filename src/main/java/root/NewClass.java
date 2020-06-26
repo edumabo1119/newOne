86cafab1b0ab471bff4d3ee1f4f08ab2f9e3739f
edu_mabo1119 <maksym.bova@gmail.com>
2020-06-27 00:45:21 +0300

    new class

---
+++ src/main/java/root/NewClass.java
@@ -0,0 +1,8 @@
+package root;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("My new line");
+        System.out.println("My new folder");
+    }
+}
