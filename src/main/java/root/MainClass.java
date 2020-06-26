02e95d2c85d255acfda1714ae6c00f650626d733
edu_mabo1119 <maksym.bova@gmail.com>
2020-06-27 00:23:45 +0300

    new line

--- src/main/java/root/MainClass.java
+++ src/main/java/root/MainClass.java
@@ -3,5 +3,6 @@ package root;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Here we go!");
+        System.out.println("There is something new!");
     }
 }
